3e7bf376e58f3ee419b5d5ae8a49acc9d25133c5
DevilRaider-Dev <deviltwix3@gmail.com>
2021-10-14 14:52:11 +0200

    add album list by artist and detailpage with iframe 2 play the album

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const port = process.env.PORT;
 
 //init spotify
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
 });
 // Retrieve an access token
 spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 //generate app und bind public folder
 const app = express();
@@ -26,36 +26,35 @@ app.set('view engine', 'ejs')
 
 //port listener
 app.listen(port, () => {
-    //ToDo: write in log
-    console.log(`listening at http://localhost:${port}`);
+  //ToDo: write in log
+  console.log(`listening at http://localhost:${port}`);
 });
 
 //Router
 app.get('/', (req, res) => {
-    res.render('index')
+  res.render('index')
 })
 
 app.get('/artist-search', (req, res) => {
-    spotifyApi.searchArtists(req.query.searchByArtist)
-        .then(function (data) {
-            res.render('pages/artist-search-results.ejs', { artists: data.body.artists })
-            //console.log(`Search artists by ${req.query.searchByArtist}`, data.body.artists);
-        }, function (err) {
-            console.error(err);
-        });
+  spotifyApi.searchArtists(req.query.searchByArtist)
+    .then(function (data) {
+      res.render('pages/artist-search-results.ejs', { artists: data.body.artists.items })
+    }, function (err) {
+      console.error(err);
+    });
 })
 
-/* spotifyApi.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
-  .then(function(data) {
-    console.log('Artist information', data.body);
-  }, function(err) {
-    console.error(err);
-  });
+app.get('/albums/:id', (req, res) => {
+  spotifyApi.getArtistAlbums(req.params.id)
+    .then(function (data) {
+      res.render('pages/albums.ejs', { album: data.body.items })
+    },
+      function (err) {
+        console.error(err);
+      }
+    );
+});
 
-  spotifyApi.searchArtists('Love')
-  .then(function(data) {
-    console.log('Search artists by "Love"', data.body);
-  }, function(err) {
-    console.error(err);
-  });
-  */
+app.get('/album/tracks/:id', (req, res) => {
+  res.render('pages/tracks.ejs', { id: req.params.id })
+})
